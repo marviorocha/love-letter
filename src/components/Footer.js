b1ac5dd42f8468c0a9bb730969c26a1f51c0d102
marviorocha <marviorocha@marviorocha.com>
2020-07-28 12:21:51 -0300

    Add first project React

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(){
+
+    return(
+ 
+      <footer>
+      <p>Happily crafted by @amberlimshin.</p>
+      </footer>
+     )
+
+
+}
+
+export default Footer
